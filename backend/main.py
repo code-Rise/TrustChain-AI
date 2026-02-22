99e506262341cbf8b01f53c56e708d0d2ddc645b
0122345 <ntwarifiacre043@gmail.com>
2026-02-22 21:34:33 +0200

    adding a port

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,11 @@ from sqlalchemy import func
 
 from db.database import SessionLocal, engine
 from models import models
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 # Create database tables
 models.Base.metadata.create_all(bind=engine)
